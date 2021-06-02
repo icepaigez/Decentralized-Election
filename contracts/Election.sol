935f15985efc61c9a6e3968dce24c3a16686b2ab
tunde <icepaigez@gmail.com>
2021-06-02 12:12:50 +0100

    setting up election rules

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,9 +1,36 @@
 pragma solidity ^0.5.0;
 
 contract Election {
-	string public candidate;
+	struct Candidate {
+		uint id;
+		string name;
+		uint voteCount;
+	}
+
+	uint public candidateCount;
+	mapping (uint => Candidate) public candidates;
+
+	//prevent double votes - voter register
+	mapping (string => address) public voterRegistry;
 
 	constructor() public {
-		candidate = "Candidate 1";
+		_addCandidate("Kanye West 001");
+		_addCandidate("Dwayne Johnson 002");
+		_addCandidate("Denzel Washington 003");
+	}
+
+	modifier singleVote(string memory _voterName) {
+		require(msg.sender != voterRegistry[_voterName]);
+		_;
+	}
+
+	function _addCandidate(string memory _name) private {
+		candidateCount++;
+		candidates[candidateCount] = Candidate(candidateCount, _name, 0);
+	}
+
+	function voteCandidate(uint _candidateId, string memory _voterName) public singleVote(_voterName) {
+		candidates[_candidateId].voteCount++;
+		voterRegistry[_voterName] = msg.sender;
 	}
 }
